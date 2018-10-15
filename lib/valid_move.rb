be3552f412040db5a74d064d87d0c122d2f77bc9
severus889700 <severus889700@gmail.com>
2018-10-15 00:49:35 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,4 +1,5 @@
-# code your #valid_move? method here
+# code your #valid_move(board)? method here
 
 
 # re-define your #position_taken? method here, so that you can use it in the #valid_move? method above.
+ def valid_move(board)
